3538c585bd161c932def7e6bffa713227827d699
KrivoshejaIvan <ranger3331@gmail.com>
2019-02-11 13:55:45 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿'USE STRICT'
+
+var simpleArr = [1,2,3,4,5,6,7]
+function square(x) {
+    return x*x
+}
+
+function map(fun, arr) {
+
+
+    var newArr = [];
+    for (var i =0; i < simpleArr.length; i++) {
+        newArr.push(square(simpleArr[i]))
+    } console.log(newArr);
+
+}
+
+console.log(map(square(), simpleArr))
